50b46e39fd7adbd4c8bfec0e13490ae436b8367d
Amit Karpe <amitkarpe@gmail.com>
2024-10-03 05:14:29 +0000

    Added code for creating S3 bucket.

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+profile = "default"
+region = "ap-southeast-1"
+}
+############ SAVING TF STATE FILE #########
+terraform {
+  backend "s3" {
+    bucket  = "atlantis-tf-state-111111"
+    key     = "atlantis/terraform.tfstate"
+    region  = "ap-southeast-1"
+    profile = "default"
+  }
+}
+resource "random_pet" "bucket_name" {
+length = 5
+separator = "-"
+}
+resource "aws_s3_bucket" "test_bucket" {
+bucket = "atlantis-test-${random_pet.bucket_name.id}"
+}
