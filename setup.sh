6fe763c6846fecd0a096fbdb1333a10c6e35edbf
Amit Karpe <amitkarpe@gmail.com>
2024-10-03 05:18:47 +0000

    Inatalled atlantis

--- setup.sh
+++ setup.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Update system and install dependencies
-sudo yum update -y
-sudo yum install -y yum-utils shadow-utils wget
-
-# Install Terraform
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
-
-# Install Atlantis
-sudo wget https://github.com/runatlantis/atlantis/releases/latest/download/atlantis_linux_amd64.tar.gz
-sudo tar -xvf atlantis_linux_amd64.tar.gz -C /usr/local/bin
-sudo rm atlantis_linux_amd64.tar.gz
-
 # Create Atlantis configuration directory
 sudo mkdir -p /etc/atlantis
 
